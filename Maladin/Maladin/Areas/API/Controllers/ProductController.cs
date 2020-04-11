22d050e191051a4848f11b9d511afca9ed064467
vutuyen <hoangtuyensk@gmail.com>
2020-04-12 04:52:23 +0700

    areas
    tuyen commit

---
+++ Maladin/Maladin/Areas/API/Controllers/ProductController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Maladin.Areas.API.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: API/Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult getAllInfomationProduct()
+        {
+            return Json("hello json from post method", JsonRequestBehavior.DenyGet);
+        }
+        [HttpPost]
+        public JsonResult insertProduct()
+        {
+            return Json("true", JsonRequestBehavior.DenyGet);
+
+        }
+        [HttpPost]
+        public JsonResult getOrigin()
+        {
+            return Json("", JsonRequestBehavior.DenyGet);
+        }
+        [HttpPost]
+        public JsonResult deleteProduct(string id)
+        {
+            return Json("delete complate "+id,JsonRequestBehavior.DenyGet);
+        }
+        [HttpPost]
+        public JsonResult editProduct(string id)
+        {
+            return Json("", JsonRequestBehavior.DenyGet);
+        }
+    }
+}
---
+++ Maladin/Maladin/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Maladin.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Admin/Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult getInfomationProduct()
+        {
+            return Json("hello json from post method", JsonRequestBehavior.DenyGet);
+        }
+
+        
+    }
+}
