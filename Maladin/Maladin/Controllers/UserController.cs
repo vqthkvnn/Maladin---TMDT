5c51d96129c469b8674acf6407acf10344796535
Hoang_Giang <hoanggiang521999@gmail.com>
2020-05-01 03:33:32 +0700

    giang commit

--- Maladin/Maladin/Controllers/UserController.cs
+++ Maladin/Maladin/Controllers/UserController.cs
@@ -33,5 +33,28 @@ namespace Maladin.Controllers
         {
             return View();
         }
+
+        public ActionResult MyNotification()        //quan ly thong bao
+        {
+            return View();
+        }
+
+        public ActionResult ManagerOrder()      //quan ly don hang
+        {
+            return View();
+        }
+
+        public ActionResult ReviewProductBought()       //nhan xet san pham da mua
+        {
+            return View();
+        }
+        public ActionResult MyReview()      //nhan xet cua toi
+        {
+            return View();
+        }
+        public ActionResult ManagerMaladinCoin()        //quan ly maladin coin
+        {
+            return View();
+        }
     }
 }
